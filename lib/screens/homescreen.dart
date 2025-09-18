62f9c0e1b77470019bb2b5b29804ceadd38f7922
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-18 14:56:16 -0600

    Se ha creado la interfaz principal para la seleccion de inicios de sesion

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -10,6 +11,258 @@ class Homescreen extends StatefulWidget {
 class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 60.0, bottom: 20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: SvgPicture.asset(
+                    'assets/images/heart.svg',
+                    height: 30,
+                    colorFilter: ColorFilter.mode(
+                      Colors.white,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 25, bottom: 10),
+                child: Text(
+                  "MediCare",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Text(
+                "Cuidado medico en tus manos",
+                style: TextStyle(fontWeight: FontWeight.normal),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30, bottom: 10),
+                child: Text(
+                  "¿Cómo quieres acceder?",
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+                ),
+              ),
+              Text(
+                "Selecciona tu tipo de usuario para continuar",
+                style: TextStyle(fontWeight: FontWeight.normal),
+              ),
+              familiarSelector(),
+              cuidadorSelector(),
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: Text(
+                  "¿Nesesitas ayuda? Contacta con soporte",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding familiarSelector() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30, left: 20, right: 20, bottom: 10),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.grey, width: .5),
+        ),
+        child: SizedBox(
+          height: 200,
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20, left: 20, bottom: 20),
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(31, 21, 93, 252),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Icon(
+                          Icons.people_outline,
+                          color: Color.fromARGB(255, 21, 93, 252),
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Soy familiar",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 222,
+                        child: Text(
+                          "Monitorea el cuidado de tu ser querido, recibe notificaciones importantes y mantente conectado con el equipo de cuidado",
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          shape: WidgetStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          backgroundColor: WidgetStatePropertyAll(Colors.black),
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Continuar como familiar",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                                size: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding cuidadorSelector() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 5),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.grey, width: .5),
+        ),
+        child: SizedBox(
+          height: 200,
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20, left: 20, bottom: 20),
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(100, 219, 252, 231),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Icon(
+                          Icons.person_2_outlined,
+                          color: Color.fromARGB(255, 0, 166, 62),
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Soy cuidador",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 222,
+                        child: Text(
+                          "Gestiona y monitorea a tus pacientes (Se requiere cuenta creada por el familiar)",
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          shape: WidgetStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          backgroundColor: WidgetStatePropertyAll(
+                            Color.fromARGB(98, 169, 172, 179),
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Continuar como cuidador",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Icon(
+                                Icons.arrow_forward,
+                                color: Colors.black,
+                                size: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
