5da5dc68e8f9e2363d227ed07a9ccf017c872e8b
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-18 18:46:28 -0600

    Se ha agregado el elemento screen para el login de los familiares

---
+++ lib/screens/familiarloginscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Familiarloginscreen extends StatefulWidget {
+  const Familiarloginscreen({super.key});
+
+  @override
+  State<Familiarloginscreen> createState() => _FamiliarloginscreenState();
+}
+
+class _FamiliarloginscreenState extends State<Familiarloginscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
