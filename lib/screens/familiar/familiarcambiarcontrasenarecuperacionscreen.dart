af7a046dcaf6168e5578c0db74753436dffbba11
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-20 11:46:58 -0600

    Se ha agregado la screen para el cambio de contraseña del familiar (verificacion) y su estructura basica

---
+++ lib/screens/familiar/familiarcambiarcontrasenarecuperacionscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Familiarcambiarcontrasenarecuperacionscreen extends StatefulWidget {
+  const Familiarcambiarcontrasenarecuperacionscreen({super.key});
+
+  @override
+  State<Familiarcambiarcontrasenarecuperacionscreen> createState() =>
+      _FamiliarcambiarcontrasenarecuperacionscreenState();
+}
+
+class _FamiliarcambiarcontrasenarecuperacionscreenState
+    extends State<Familiarcambiarcontrasenarecuperacionscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
