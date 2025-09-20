d07783eaa8e304516c52fe736b0ae86dddd1fe10
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-20 11:03:54 -0600

    Se ha agregado la estructura al elemento de la pantalla para verificar codigo de los familiares

---
+++ lib/screens/familiar/familiarverificarcodigoscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Familiarverificarcodigoscreen extends StatefulWidget {
+  const Familiarverificarcodigoscreen({super.key});
+
+  @override
+  State<Familiarverificarcodigoscreen> createState() =>
+      _FamiliarverificarcodigoscreenState();
+}
+
+class _FamiliarverificarcodigoscreenState
+    extends State<Familiarverificarcodigoscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
