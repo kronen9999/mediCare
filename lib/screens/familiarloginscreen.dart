c85f5730f1ff5bca6cacdae3ddfac5cc74ae1459
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-19 22:35:13 -0600

    Se ha agregado la estructura de la screen del inicio de sesion del familiar

--- lib/screens/familiarloginscreen.dart
+++ lib/screens/familiarloginscreen.dart
@@ -46,26 +46,194 @@ class _FamiliarloginscreenState extends State<Familiarloginscreen> {
       body: SizedBox(
         width: double.infinity,
         height: double.infinity,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(31, 21, 93, 252),
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Icon(
-                    Icons.people_outline,
-                    color: Color.fromARGB(255, 21, 93, 252),
-                    size: 40,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(31, 21, 93, 252),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Icon(
+                      Icons.people_outline,
+                      color: Color.fromARGB(255, 21, 93, 252),
+                      size: 40,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 10),
+                child: Text(
+                  "Ingresar como familiar",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                "Ingresa tus credenciales para acceder",
+                style: TextStyle(fontSize: 15, color: Colors.grey),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.grey, width: .5),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            "Usuario o Correo Electrónico",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Ingresa tu usuario o correo",
+                                prefixIcon: Icon(Icons.person_outline),
+                                filled: true,
+                                fillColor: const Color.fromARGB(
+                                  59,
+                                  180,
+                                  180,
+                                  182,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Text(
+                              "Contraseña",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 55,
+
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                hintText: "Ingresa tu contraseña",
+                                prefixIcon: Icon(Icons.lock_person_outlined),
+                                filled: true,
+                                fillColor: const Color.fromARGB(
+                                  59,
+                                  180,
+                                  180,
+                                  182,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ButtonStyle(
+                                shape: WidgetStatePropertyAll(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                backgroundColor: WidgetStatePropertyAll(
+                                  Colors.black,
+                                ),
+                              ),
+                              child: Text(
+                                "Iniciar sesion",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Divider(color: Colors.grey, height: 80),
+                        Text("¿No tienes una cuenta?"),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 5, bottom: 5),
+                            child: TextButton(
+                              onPressed: () {},
+                              style: ButtonStyle(
+                                shape: WidgetStatePropertyAll(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    side: BorderSide(
+                                      color: Colors.grey,
+                                      width: 1,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              child: Text(
+                                "Regístrate aquí",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "¿Olvidaste tu contraseña? Recupérala aquí",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
