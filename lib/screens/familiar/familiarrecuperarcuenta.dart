79d6bf343cc714bf3878cc7f5331e27fe3b60dfb
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-20 08:07:22 -0600

    Se ha agregado la estructura basica

--- lib/screens/familiar/familiarrecuperarcuenta.dart
+++ lib/screens/familiar/familiarrecuperarcuenta.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+class Familiarrecuperarcuenta extends StatefulWidget {
+  const Familiarrecuperarcuenta({super.key});
+
+  @override
+  State<Familiarrecuperarcuenta> createState() =>
+      _FamiliarrecuperarcuentaState();
+}
+
+class _FamiliarrecuperarcuentaState extends State<Familiarrecuperarcuenta> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
