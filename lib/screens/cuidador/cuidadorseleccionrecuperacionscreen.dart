9de0c9401e3d422faa042c3863bcaf23ea0497bf
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-20 14:53:26 -0600

    Se ha agregado la pantalla de seleccion de recuperacion de los cuidadores

---
+++ lib/screens/cuidador/cuidadorseleccionrecuperacionscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Cuidadorseleccionrecuperacionscreen extends StatefulWidget {
+  const Cuidadorseleccionrecuperacionscreen({super.key});
+
+  @override
+  State<Cuidadorseleccionrecuperacionscreen> createState() =>
+      _CuidadorseleccionrecuperacionscreenState();
+}
+
+class _CuidadorseleccionrecuperacionscreenState
+    extends State<Cuidadorseleccionrecuperacionscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
