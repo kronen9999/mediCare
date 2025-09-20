645e64a8c31ad97895c98b4b67f1b0d8cfa9884d
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-20 12:02:23 -0600

    Se ha agregado  la pantalla para que el familiar pueda hacer su registro y la estructura basica

---
+++ lib/screens/familiar/familiarregistroscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Familiarregistroscreen extends StatefulWidget {
+  const Familiarregistroscreen({super.key});
+
+  @override
+  State<Familiarregistroscreen> createState() => _FamiliarregistroscreenState();
+}
+
+class _FamiliarregistroscreenState extends State<Familiarregistroscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
