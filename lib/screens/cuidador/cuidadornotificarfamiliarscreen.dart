21a00891acc08c2784bd2797188ec33b446f0ade
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-21 21:20:19 -0600

    Se ha agregado la pantalla y la estructura basica del la pantalla para notificar al familiar

---
+++ lib/screens/cuidador/cuidadornotificarfamiliarscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Cuidadornotificarfamiliarscreen extends StatefulWidget {
+  const Cuidadornotificarfamiliarscreen({super.key});
+
+  @override
+  State<Cuidadornotificarfamiliarscreen> createState() =>
+      _CuidadornotificarfamiliarscreenState();
+}
+
+class _CuidadornotificarfamiliarscreenState
+    extends State<Cuidadornotificarfamiliarscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
