863641383f7260fd997284bc9976cdb7a23bb524
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-21 21:01:16 -0600

    Se ha agregado la estructura basica para la pantalla de verificar codigo de el cuidador

---
+++ lib/screens/cuidador/cuidadorverificarcodigoscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Cuidadorverificarcodigoscreen extends StatefulWidget {
+  const Cuidadorverificarcodigoscreen({super.key});
+
+  @override
+  State<Cuidadorverificarcodigoscreen> createState() =>
+      _CuidadorverificarcodigoscreenState();
+}
+
+class _CuidadorverificarcodigoscreenState
+    extends State<Cuidadorverificarcodigoscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
