a88d13fac85cae2bb03a9737051f6890377d81cd
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-21 21:10:15 -0600

    Se ha agregado la pantalla de cambiar la contraseña por recuperacion del cuidador y su estructura basica

---
+++ lib/screens/cuidador/cuidadorcambiarcontrasenarecuperacionscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Cuidadorcambiarcontrasenarecuperacionscreen extends StatefulWidget {
+  const Cuidadorcambiarcontrasenarecuperacionscreen({super.key});
+
+  @override
+  State<Cuidadorcambiarcontrasenarecuperacionscreen> createState() =>
+      _CuidadorcambiarcontrasenarecuperacionscreenState();
+}
+
+class _CuidadorcambiarcontrasenarecuperacionscreenState
+    extends State<Cuidadorcambiarcontrasenarecuperacionscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
