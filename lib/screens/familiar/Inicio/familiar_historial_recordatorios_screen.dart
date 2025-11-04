e18025cbbd21a51d8d80484191efb645912c76af
kronen9999 <stacica22eneronkio@gmail.com>
2025-11-04 15:13:55 -0600

    Se ha agregado la pantalla para el historial de administraciones

---
+++ lib/screens/familiar/Inicio/familiar_historial_recordatorios_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FamiliarHistorialRecordatoriosScreen extends StatefulWidget {
+  const FamiliarHistorialRecordatoriosScreen({super.key});
+
+  @override
+  State<FamiliarHistorialRecordatoriosScreen> createState() =>
+      _FamiliarHistorialRecordatoriosScreenState();
+}
+
+class _FamiliarHistorialRecordatoriosScreenState
+    extends State<FamiliarHistorialRecordatoriosScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
