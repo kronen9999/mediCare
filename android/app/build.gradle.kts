5c35b32e7fab53ddd0e59ce52a713ed036101d7f
kronen9999 <stacica22eneronkio@gmail.com>
2025-11-11 09:49:29 -0600

    Se han agregado paquetes de timezone,y los permisos para las notificaciones en android e ios

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -41,4 +42,8 @@ android {
 
 flutter {
     source = "../.."
+}
+// Dependencias necesarias para core library desugaring
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
