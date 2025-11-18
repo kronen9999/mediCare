379a597fa93892a46c186f8e698b39afb8da77fd
kronen9999 <stacica22eneronkio@gmail.com>
2025-11-18 14:46:44 -0600

    Se han agregado validaciones  de solicitud a las pantallas de  registro,cambio de contraseña,verificacion de codigo y recuperar cuenta de los familiares

--- lib/screens/familiar/familiarverificarcodigoscreen.dart
+++ lib/screens/familiar/familiarverificarcodigoscreen.dart
@@ -218,9 +218,14 @@ class _FamiliarverificarcodigoscreenState
       );
     } catch (e) {
       Navigator.of(context).pop();
+      String message = e.toString();
+      if (message.startsWith("ClientException")) {
+        message =
+            "Error de conexión. Por favor, verifica tu conexión a internet e intentalo de nuevo.";
+      }
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text(e.toString().replaceAll('Exception: ', '')),
+          content: Text(message.replaceAll('Exception: ', '')),
           backgroundColor: Colors.red,
         ),
       );
@@ -254,9 +259,14 @@ class _FamiliarverificarcodigoscreenState
       );
     } catch (e) {
       Navigator.of(context).pop();
+      String message = e.toString();
+      if (message.startsWith("ClientException")) {
+        message =
+            "Error de conexión. Por favor, verifica tu conexión a internet e intentalo de nuevo.";
+      }
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text(e.toString().replaceAll('Exception: ', '')),
+          content: Text(message.replaceAll('Exception: ', '')),
           backgroundColor: Colors.red,
         ),
       );
