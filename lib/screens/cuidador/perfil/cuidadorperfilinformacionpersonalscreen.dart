b29a97ffdb24f6ae3b9ad9884100ea8488fc5fd0
kronen9999 <stacica22eneronkio@gmail.com>
2025-11-18 17:13:51 -0600

    Se han agregado validaciones de solicitud a  editar informacion personal,de la cuenta y cambiar la contraseña

--- lib/screens/cuidador/perfil/cuidadorperfilinformacionpersonalscreen.dart
+++ lib/screens/cuidador/perfil/cuidadorperfilinformacionpersonalscreen.dart
@@ -598,13 +598,19 @@ class _CuidadorperfilinformacionpersonalscreenState
       );
       widget.onSelection("default");
     } catch (e) {
-      if (mounted) {
-        Navigator.of(context).pop();
+      if (!mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
+      String message = e.toString();
+      if (message.startsWith("ClientException")) {
+        message =
+            "Error de conexión. Por favor, verifica tu conexión a internet e intentalo de nuevo.";
       }
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           backgroundColor: Colors.red,
-          content: Text(e.toString().replaceAll("Exception: ", "")),
+          content: Text(message.toString().replaceAll("Exception: ", "")),
         ),
       );
     }
